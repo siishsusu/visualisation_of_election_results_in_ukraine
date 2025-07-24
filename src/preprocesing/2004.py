a7749d6cc632eab2dbd5cb3dae64a0b9de393bc7
siishsusu <byvladyslava@gmail.com>
2025-07-24 19:31:09 +0300

    preprocessed regions file for 2004

--- src/preprocesing/2004.py
+++ src/preprocesing/2004.py
@@ -38,6 +38,38 @@ candidates = {
 }
 
 
+regions_translation = {
+    'м.Київ': 'Kyiv',
+    'Київська': 'Kyivska',
+    'Харківська': 'Kharkivska',
+    'Дніпропетровська': 'Dnipropetrovska',
+    'Черкаська': 'Cherkaska',
+    'Закарпатська': 'Zakarpatska',
+    'Рівненська': 'Rivnenska',
+    'Одеська': 'Odeska',
+    'Полтавська': 'Poltavska',
+    'Чернівецька': 'Chernivetska',
+    'Хмельницька': 'Khmelnytska',
+    'Житомирська': 'Zhytomyrska',
+    'Волинська': 'Volynska',
+    'Запорізька': 'Zaporizka',
+    'Чернігівська': 'Chernihivska',
+    'Миколаївська': 'Mykolaivska',
+    'Вінницька': 'Vinnytska',
+    'Львівська': 'Lvivska',
+    'Кіровоградська': 'Kirovohradska',
+    'Херсонська': 'Khersonska',
+    'Тернопільська': 'Ternopilska',
+    'Сумська': 'Sumska',
+    'Луганська': 'Luhanska',
+    'Івано-Франківська': 'Ivano-Frankivska',
+    'Донецька': 'Donetska',
+    'Авт.Респ. Крим': 'Avtonomna Respublika Krym',
+    'Автономна Республіка Крим': 'Avtonomna Respublika Krym',
+    'м.Севастополь': 'Sevastopilska',
+}
+
+
 # final data preprocessing
 df_final = pd.read_csv(f'{base_data_path}{data_path["final"]}')
 # renaming columns
@@ -64,3 +96,39 @@ if '2004' not in os.listdir('data/preprocessed'):
 
 # saving file to folder 
 df_final.to_csv('data/preprocessed/2004/final_2004.csv', index=False)
+
+
+# -------------- regions data preprocessing --------------
+df_regions = pd.read_csv(f'{base_data_path}{data_path["regions"]}')
+
+# renaming columns
+df_regions.columns = ['region', 'percent_votes', 'rating',
+       'num_voters_per_region',
+       'percent_processed_votes', 'candidate']
+
+#   num_voters_per_region should be cast as 'int64'
+# example of data in column 'num_voters_per_region': '1 324 847',
+#   so, firstly I'm replacing spaces to nothing
+df_regions['num_voters_per_region'] = df_regions['num_voters_per_region']\
+    .apply(lambda x: int(x.replace(' ', '')))
+
+# print(df_regions['percent_processed_votes'].unique())
+#   [100.]  →   so, I'm dropping this column
+df_regions.drop(['percent_processed_votes'], axis=1, inplace=True)
+
+# print(df_regions.columns[df_regions.isna().sum() > 0]) →
+#   Index([], dtype='object')
+
+# print(df_regions['region'].unique())
+
+df_regions['candidate'] = df_regions['candidate'].apply(lambda x: candidates[f'{x.replace(".", ". ")}.'])
+
+# creating translated column
+df_regions['region_eng'] = df_regions['region'].apply(lambda x: regions_translation[x])
+
+# changing the order of columns
+df_regions = df_regions[['candidate', 'region', 'region_eng', 'percent_votes', 'rating', 
+                         'num_voters_per_region']]
+
+# saving to file
+df_regions.to_csv('data/preprocessed/2004/regions_2004.csv', index=False)
